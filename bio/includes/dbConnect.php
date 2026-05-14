1edb3222c0c77b783990b560ff745814a72b7753
wbh <wajihbenhassen@gmail.com>
2026-05-14 08:36:04 -0400

    change config

--- bio/includes/dbConnect.php
+++ bio/includes/dbConnect.php
@@ -4,9 +4,6 @@ $configFile = dirname(__DIR__, 2) . '/includes/db_config.local.php';
 if (is_readable($configFile)) {
     /** @var array{host:string,user:string,password:string,database:string} $dbConfig */
     $dbConfig = require $configFile;
-} else {
-    /** @var array{host:string,user:string,password:string,database:string} $dbConfig */
-    $dbConfig = require dirname(__DIR__, 2) . '/includes/db_config.local.example.php';
 }
 
 $con = @mysqli_connect(
